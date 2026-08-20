9c149e29292d4a057b47c02d58bc4d67fef68afb
redavillm <leonofforc@yandex.ru>
2026-08-20 23:32:24 +0300

    added background img and made auth page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,5 +1,16 @@
+import styles from './Auth.module.css'
+
 export const Auth = () => {
     return (
-        <div>Authorization Page</div>
+        <div>
+            <h1 className={styles.title}>Хронинки Этельгарда</h1>
+            <div className={styles.authForm}>
+                <label htmlFor="username">Email/Логин</label>
+                <input id="username" type="text" placeholder="Username" />
+                <label htmlFor="password">Пароль</label>
+                <input id="password" type="password" placeholder="Password" />
+                <button type="submit">Войти</button>
+            </div>
+        </div>
     )
 }
